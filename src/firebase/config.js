87ae68db2b75b54648617400102ae60c067f5125
Shubham Gandhi <shubhamrgandhi@gmail.com>
2020-12-30 00:41:56 +0530

    fire

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: "AIzaSyBlu5CJH3fMbYi2HI_zvBpqDtmDK6hEOyE",
+  authDomain: "confusionserver-82b27.firebaseapp.com",
+  databaseURL: "https://confusionserver-82b27.firebaseio.com",
+  projectId: "confusionserver-82b27",
+  storageBucket: "confusionserver-82b27.appspot.com",
+  messagingSenderId: "1073234470637",
+  appId: "1:1073234470637:web:0f8e073b34493d92f4cee4",
+  measurementId: "G-DMVJ7M0ZJB"
+};
---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+import { config } from './config';
+import firebase from 'firebase';
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+
+export const fireauth = firebase.auth;
+
+firebase.firestore();
+export const firestore = firebase.firestore();
+
+export const firebasestore = firebase.firestore;
